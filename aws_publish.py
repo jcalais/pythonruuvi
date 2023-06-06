9dfc20c02c447e582fa90a3fbb9ce9012d557094
Joakim Calais <joakim.calais@gmail.com>
2023-06-06 23:25:24 +0300

    Added aws publisher.

---
+++ aws_publish.py
@@ -0,0 +1,22 @@
+# This script is designed to be called from crontab. 
+# It will publish to AWS periodically.
+import boto3
+import os
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from influxdb import InfluxDBClient
+
+load_dotenv()
+
+
+def main():
+    fivemins_datetime = datetime.datetime.now() - datetime.timedelta(minutes=5)
+    fivemins = fivemins_datetime.strftime('%Y-%m-%d %H:%M:%S')
+    client = InfluxDBClient(host=os.getenv('INFLUX_HOST'), port=os.getenv('INFLUX_PORT'), database=os.getenv('INFLUX_DB'))
+    query = f"select temperature,mac from ruuvi_measurements where time > '{fivemins}' limit 20"
+    params = {'host': 'server01'}
+    result = client.query(query, bind_params=params)
+    print(result)
+
+if __name__ == "__main__":
+    main()
