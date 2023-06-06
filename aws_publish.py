faae4fa0bf1b81d3afb32760d26baf0ad541ed20
Joakim Calais <joakim.calais@gmail.com>
2023-06-06 23:44:24 +0300

    Testing influx query.

--- aws_publish.py
+++ aws_publish.py
@@ -7,18 +7,24 @@ from dotenv import load_dotenv
 from influxdb import InfluxDBClient
 
 load_dotenv()
+TIME_ZONE = os.getenv('TIMEZONE', 0)
 
 def main():
-    fivemins_datetime = datetime.now() - timedelta(minutes=5, hours=3)
+    fivemins_datetime = datetime.now() - timedelta(minutes=5, hours=TIME_ZONE)
     fivemins = fivemins_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    print(fivemins)
     client = InfluxDBClient(host=os.getenv('INFLUX_HOST'), port=os.getenv('INFLUX_PORT'), database=os.getenv('INFLUX_DB'))
-    query = f"select temperature,mac from ruuvi_measurements where time > '{fivemins}' limit 20"
+    query = f"select temperature,mac from ruuvi_measurements where time > '{fivemins}' order by time desc limit 20"
     params = {'host': 'server01'}
+    
+    metrics_data = {}
     result = client.query(query, bind_params=params)
     for row in result:
-        print(row)
-
+        if row[mac] not in metrics_data:
+            metrics_data[row["mac"]] = {
+                "time": row["time"],
+                "temperature": row["temperature"]
+            }
+    print(metrics_data)
 
 if __name__ == "__main__":
     main()
